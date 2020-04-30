d901d49c38a08cb4f12fb4bd2ba2ed7ce011e160
Lian <liulian1004@hotmail.com>
2020-04-29 20:53:53 -0700

    new react project

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+//virtual dom transfer to dom
 ReactDOM.render(
   <React.StrictMode>
     <App />
